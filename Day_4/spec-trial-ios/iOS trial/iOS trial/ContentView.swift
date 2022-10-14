29297ff269aed02ebada2ca04c70d70bcce0794b
Collin Rentz <rentzcollin@gmail.com>
2022-10-14 10:46:33 -0600

    assessment 5

---
+++ Day_4/spec-trial-ios/iOS trial/iOS trial/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS trial
+//
+//  Created by Collin Rentz on 10/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day_4/spec-trial-ios/iOS trial/iOS trial/iOS_trialApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_trialApp.swift
+//  iOS trial
+//
+//  Created by Collin Rentz on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_trialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
